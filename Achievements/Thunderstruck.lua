787e6175c97adbaf7cbbabb62234e24cbff275d8
ennvina <vinnywow@aim.com>
2023-05-23 19:06:23 +0200

    Translate Thunderstruck spells from Wowhead Italian is not translated because Wowhead Classic lacks Italian support More exactly, Italian texts are copied from English

--- Achievements/Thunderstruck.lua
+++ Achievements/Thunderstruck.lua
@@ -58,6 +58,75 @@ local blacklist = {
 	["Choque de Escarcha"] = 1,
 	["Arma Estigma de Escarcha"] = 1,
 
+	-- Same list, now in Italian
+	-- Forbidden fire spells
+--	["Fire Nova Totem"] = 1,
+--	["Flame Shock"] = 1,
+--	["Flametongue Totem"] = 1,
+--	["Flametongue Weapon"] = 1,
+--	["Frost Resistance Totem"] = 1,
+--	["Magma Totem"] = 1,
+--	["Searing Totem"] = 1,
+	-- Forbidden frost spells
+--	["Fire Resistance Totem"] = 1,
+--	["Frost Shock"] = 1,
+--	["Frostbrand Weapon"] = 1,
+
+	-- Same list, now in Portuguese
+	-- Forbidden fire spells
+	["Totem de Nova de Fogo"] = 1,
+	["Choque Flamejante"] = 1,
+	["Totem de Labaredas"] = 1,
+	["Arma de Labaredas"] = 1,
+	["Totem de Resistência ao Gelo"] = 1,
+--	["Totem de Magma"] = 1, -- Same as French
+	["Totem Calcinante"] = 1,
+	-- Forbidden frost spells
+	["Totem de Resistência ao Fogo"] = 1,
+	["Choque Gélido"] = 1,
+	["Arma da Marca Gélida"] = 1,
+
+	-- Same list, now in Russian
+	-- Forbidden fire spells
+	["Тотем кольца огня"] = 1,
+	["Огненный шок"] = 1,
+	["Тотем языка пламени"] = 1,
+	["Оружие языка пламени"] = 1,
+	["Тотем сопротивления льду"] = 1,
+	["Тотем магмы"] = 1,
+	["Опаляющий тотем"] = 1,
+	-- Forbidden frost spells
+	["Тотем сопротивления огню"] = 1,
+	["Ледяной шок"] = 1,
+	["Оружие ледяного клейма"] = 1,
+
+	-- Same list, now in Korean
+	-- Forbidden fire spells
+	["불꽃 회오리 토템"] = 1,
+	["화염 충격"] = 1,
+	["불꽃의 토템"] = 1,
+	["불꽃의 무기"] = 1,
+	["냉기 저항 토템"] = 1,
+	["용암 토템"] = 1,
+	["불타는 토템"] = 1,
+	-- Forbidden frost spells
+	["화염 저항 토템"] = 1,
+	["냉기 충격"] = 1,
+	["냉기의 무기"] = 1,
+
+	-- Same list, now in Chinese
+	-- Forbidden fire spells
+	["火焰新星图腾"] = 1,
+	["烈焰震击"] = 1,
+	["火舌图腾"] = 1,
+	["火舌武器"] = 1,
+	["抗寒图腾"] = 1,
+	["熔岩图腾"] = 1,
+	["灼热图腾"] = 1,
+	-- Forbidden frost spells
+	["抗火图腾"] = 1,
+	["冰霜震击"] = 1,
+	["冰封武器"] = 1,
 }
 
 
